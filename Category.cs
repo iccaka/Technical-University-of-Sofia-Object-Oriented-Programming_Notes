b111b383846cd3f728a48823fea6ee1f9d8f9f7b
iccaka <dakillaroshun@gmail.com>
2020-04-27 02:17:45 +0300

    Add additional Note and Category classes with getters and setters inside

---
+++ Category.cs
@@ -0,0 +1,6 @@
+﻿namespace Notes
+{
+    internal class Category
+    {
+    }
+}
---
+++ CategoryNameException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Notes
+{
+    [Serializable]
+    internal class CategoryNameException : Exception
+    {
+        public CategoryNameException()
+        {
+        }
+
+        public CategoryNameException(string message) : base(message)
+        {
+        }
+
+        public CategoryNameException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CategoryNameException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
