bc9f0f6e9e2577f8d448aec8921f5edf4afa7bb4
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2020-06-11 21:37:02 +0300

    Rename 'HeadingException.cs' to 'NoteHeadingException.cs'

---
+++ NoteHeadingException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Notes
+{
+    [Serializable]
+    internal class NoteHeadingException : Exception
+    {
+        public NoteHeadingException()
+        {
+        }
+
+        public NoteHeadingException(string message) : base(message)
+        {
+        }
+
+        public NoteHeadingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NoteHeadingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
